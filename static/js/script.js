609b27c3dc6c6f697e6c659595d362dad4cd1b8d
Evans kumi <kwakukumi14@gmail.com>
2024-11-04 02:58:27 +0000

    up me evans

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,14 @@
-document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
-      card.classList.add('clicked');
-    });
-  });
-  
+// // Add 'clicked' class to each card when clicked
+// document.querySelectorAll('.card').forEach(card => {
+//   card.addEventListener('click', () => {
+//       card.classList.toggle('clicked');
+      
+//   });
+// });
+
+// // Toggle visibility of the contact card
+// document.getElementById('getContactBtn').onclick = function() {
+//   const contactCard = document.getElementById('contactCard');
+//   contactCard.classList.toggle('d-none');  // Toggle visibility using a CSS class
+//   console.log("contactCard.classList");
+// };
