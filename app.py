c347b845d16fc5c6dff6d0d107182289bb5dcdf7
Godfred Kogkane <123625779+godfredkogkane@users.noreply.github.com>
2024-11-04 22:14:09 +0000

    Commit changes before switching branches

--- app.py
+++ app.py
@@ -2,13 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Home route
+# Home page route
 @app.route('/')
-def home():
-    return render_template('base.html')  # Link to the team’s landing page or index
-
-# Godfred's page route
-@app.route('/godfred')
 def godfred():
     return render_template('godfred_page.html')
 
