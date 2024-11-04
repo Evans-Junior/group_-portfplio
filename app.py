9ef81535565589283ab9ecada802e8cbdd0e0466
Evans kumi <kwakukumi14@gmail.com>
2024-11-04 15:47:30 +0000

    adding python4

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import os
-import yaml
+# import yaml
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # Load configuration from YAML file
-def load_config():
-    with open("config.yaml", 'r') as file:
-        config = yaml.safe_load(file)
-    return config
-
-config = load_config()
-app.config.update(config['app'])
+# def load_config():
+#     with open("config.yaml", 'r') as file:
+#         config = yaml.safe_load(file)
+#     return config
+# 
+# config = load_config()
+# app.config.update(config['app'])
 
 @app.route('/')
 def home():
@@ -30,4 +30,4 @@ def elizabeth_page():
     return render_template('project4.html')
 
 if __name__ == "__main__":
-    app.run(debug=app.config['debug'])
+    app.run()
